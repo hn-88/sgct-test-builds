a3e1bd23d52d0ccb781f1ead69922eb52bf11474
hn-88 <hn-88@users.noreply.github.com>
2025-05-26 18:52:15 +0530

    added format_to_n also to sgctcompat namespace

--- include/sgct/format_compat.h
+++ include/sgct/format_compat.h
@@ -5,16 +5,19 @@
 #include <fmt/format.h>
 namespace sgctcompat {
     using fmt::format;
+    using fmt::format_to_n;
 }
 #elif defined(__cpp_lib_format) && __cpp_lib_format >= 201907L
 // Use std::format if available
 #include <format>
 namespace sgctcompat {
     using std::format;
+    using std::format_to_n;
 }
 #else
 #include <fmt/format.h>
 namespace sgctcompat {
     using fmt::format;
+    using fmt::format_to_n;
 }
 #endif
